65aafbd1a8586dee079d93c41ecbbd93318ef61d
suzp1984 <suzp1984@gmail.com>
2015-08-24 14:46:36 +0800

    add the basic ui layout

--- app/src/main/java/lib/jacob/org/jbigandroid/BaseDrawerActivity.java
+++ app/src/main/java/lib/jacob/org/jbigandroid/BaseDrawerActivity.java
@@ -1,36 +1,46 @@
 package lib.jacob.org.jbigandroid;
 
-import android.support.design.widget.TabLayout;
-import android.support.v4.view.ViewPager;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
+import android.text.Layout;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.FrameLayout;
 
 import butterknife.Bind;
+import butterknife.ButterKnife;
 
-public class MainActivity extends BaseDrawerActivity {
+public abstract class BaseDrawerActivity extends AppCompatActivity {
 
-    @Bind(R.id.viewpager)
-    ViewPager mViewPager;
 
-    @Bind(R.id.tabs)
-    TabLayout mTabLayout;
+    @Bind(R.id.drawer_layout)
+    DrawerLayout mDrawerLayout;
 
-    @Bind(R.id.toolbar)
-    Toolbar mToolbar;
+    @Bind(R.id.nav_view)
+    NavigationView mNavigationView;
+
+    @Override
+    public void setContentView(int layoutId) {
+        super.setContentView(R.layout.activity_base_drawer);
+        FrameLayout parent = (FrameLayout) findViewById(R.id.content);
+        LayoutInflater.from(this).inflate(layoutId, parent, true);
+
+        injectView();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.viewpage_sample);
+        // setContentView(R.layout.activity_base_drawer);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_base_drawer, menu);
         return true;
     }
 
@@ -47,5 +57,9 @@ public class MainActivity extends BaseDrawerActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void injectView() {
+        ButterKnife.bind(this);
     }
 }

7b41e8fc40eefcd63d6a751e0f4f2a88b21ff7c2
suzp1984 <suzp1984@gmail.com>
2016-08-25 08:06:52 +0800

    boot agera reactive sample

---
+++ agera-sample/src/androidTest/java/suzp1984/github/io/agera_sample/ApplicationTest.java
@@ -0,0 +1,13 @@
+package suzp1984.github.io.agera_sample;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
